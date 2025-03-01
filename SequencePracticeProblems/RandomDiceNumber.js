e0b2fdbd1b03db9fc9cc0895097e183bfce01694
sganeshwani <ganeshwani2003@gmail.com>
2025-03-01 22:03:00 +0530

    [Sahil Ganeshwani] Add Random Dice Number between 1 and 6

---
+++ SequencePracticeProblems/RandomDiceNumber.js
@@ -0,0 +1,5 @@
+// Generate a random dice number between 1 and 6
+let diceNumber = Math.floor(Math.random()*6)+1;
+
+// Print the generated dice number
+console.log("You rolled a " + diceNumber + "!");
